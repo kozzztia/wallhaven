2a6d3b348fcac1cf9b0d30770b8528d1e46a4575
kozzztia <kozzztia@gmail.com>
2024-10-13 13:44:36 +0300

    click out of links

--- src/components/Layout/Controllers/Controllers.jsx
+++ src/components/Layout/Controllers/Controllers.jsx
@@ -6,15 +6,15 @@ import HeaderLink from '../HeaderLink/HeaderLink';
 import Search from "../From/Search";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useLocation } from "react-router-dom";
 import { findCurrentColor } from '../../../helpers';
 
-
-
 const Controllers = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
+    const menuRef = useRef(null); // Ссылка на элемент меню
+
     useEffect(() => {
         const handleResize = () => {
             if (window.innerWidth < 980) {
@@ -26,10 +26,25 @@ const Controllers = () => {
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            // Проверяем, был ли клик за пределами меню
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className={styles.controllers}>
+        <div className={styles.controllers} ref={menuRef}>
             <Logo />
             <button className={styles.burger} onClick={() => setIsOpen(!isOpen)} style={{color: findCurrentColor(pathname) }}>
                 <FontAwesomeIcon icon={faBars} />
@@ -37,20 +52,22 @@ const Controllers = () => {
             {
                 links.map(link =>
                     <HeaderLink 
-                    key={link.id} 
-                    href={link.href} 
-                    className={`${isOpen ? styles.linkActive : ""} ${styles.link}`} 
-                    text={link.text} 
-                    color={link.color} 
-                    index={link.id}/>
+                        key={link.id} 
+                        href={link.href} 
+                        className={`${isOpen ? styles.linkActive : ""} ${styles.link}`} 
+                        text={link.text} 
+                        color={link.color} 
+                        index={link.id} 
+                    />
                 )
             }
             <Search />
         </div>
-    )
+    );
 }
+
 Controllers.propTypes = {
     className: PropTypes.string,
 }
 
-export default Controllers
+export default Controllers;
