5b18ca375421237c48c4a9a0467cc4c9d0c3e816
kozzztia <kozzztia@gmail.com>
2024-10-13 17:07:19 +0300

    test placeholder

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,23 +7,46 @@ import { useSearchParams } from "react-router-dom";
 const Home = () => {
     const [params] = useSearchParams();
     const [posts, setPosts] = useState([]);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     useEffect(() => {
-        console.log('posts')
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                setPosts(data); // Сохраняем полученные посты в состоянии
+            } catch (error) {
+                setError(error.message); // Устанавливаем ошибку в состояние
+            } finally {
+                setLoading(false); // Устанавливаем загрузку в false после завершения
+            }
+        };
+
+        fetchPosts();
     }, [params]);
 
-    // if (loading) {
-    //     return <div>Loading...</div>; // Индикатор загрузки
-    // }
+    if (loading) {
+        return <div>Loading...</div>; // Индикатор загрузки
+    }
 
-    // if (error) {
-    //     return <div>Error: {error}</div>; // Показ ошибки
-    // }
+    if (error) {
+        return <div>Error: {error}</div>; // Показ ошибки
+    }
 
     return (
         <section className={styles.home}>
             <h1>Posts</h1>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 };
